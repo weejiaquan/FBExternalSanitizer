cd4e08d7ebb75649c6979a40cea95b2cdfe63c56
Lychwee <weejiaquan1234@gmail.com>
2020-02-29 01:16:11 -0800

    reupload

---
+++ FBSanitizer.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         FBSanitizer
+// @namespace    arisu.ml
+// @version      0.1
+// @description  Remove that stupid fbclid so u can share link wit hfriends
+// @author       Lychwee
+// @include      *
+// @grant        none
+// @run-at document-start
+// ==/UserScript==
+window.onload = function() {
+    var url = window.location.href;
+    if (window.location.href.indexOf("utm_source=") != -1) {
+        url = url.split("utm_source=")[0].slice(0, -1);
+        window.location.href = url;
+    }
+    if (window.location.href.indexOf("fbclid=") != -1) {
+        url = url.split("fbclid=")[0].slice(0, -1);
+        window.location.href = url;
+    }
+}
